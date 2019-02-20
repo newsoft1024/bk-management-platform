2508006deb36cea07978524ba9179b50629d41f1
newsoft1024 <10471715@qq.com>
2019-02-20 17:00:42 +0800

    user-service基础搭建

---
+++ user-service/src/main/java/com/newsoft1024/bkmanagementplatform/userservice/UserServiceApplication.java
@@ -0,0 +1,14 @@
+package com.newsoft1024.bkmanagementplatform.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
+
