998a48681f7e00183f19cb379840aa3d31164f0a
newsoft1024 <10471715@qq.com>
2019-02-20 17:01:54 +0800

    console-web基础搭建

---
+++ console-web/src/main/java/com/newsoft1024/bkmanagementplatform/consoleweb/ConsoleWebApplication.java
@@ -0,0 +1,13 @@
+package com.newsoft1024.bkmanagementplatform.consoleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoleWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsoleWebApplication.class, args);
+    }
+
+}
